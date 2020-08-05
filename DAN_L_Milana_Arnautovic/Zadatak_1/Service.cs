9764c77a9c60c3678d213d4d4cd40b73ae5b128e
Milana8 <91amilana@gmail.com>
2020-08-05 13:00:02 +0200

    Service, LoginView, LoginViewModel

---
+++ DAN_L_Milana_Arnautovic/Zadatak_1/Service.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Model;
+
+namespace Zadatak_1
+{
+    public class Service
+    {
+        public List<tblUser> GetAllUsers()
+        {
+            try
+            {
+                using (DAN_LEntities context = new DAN_LEntities())
+                {
+                    List<tblUser> users = new List<tblUser>();
+                    users = context.tblUsers.ToList();
+                    return users;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public List<vwSong> GetAllSong()
+        {
+            try
+            {
+                using (DAN_LEntities context = new DAN_LEntities())
+                {
+                    List<vwSong> songs = new List<vwSong>();
+                    songs = (from x in context.vwSongs select x).ToList();
+                    return songs;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+       
+    }
+}
+    
---
+++ DAN_L_Milana_Arnautovic/Zadatak_1/ViewModel/LoginViewModel.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.Model;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class LoginViewModel : ViewModelBase
+    {
+        LoginView view;
+        Service service = new Service();
+
+        #region Constructors
+
+        public LoginViewModel(LoginView view)
+        {
+            this.view = view;
+            user = new tblUser();
+            UserList = service.GetAllUsers().ToList();
+
+        }
+        #endregion
+
+        #region Property
+
+        private string userName;
+        public string UserName
+        {
+
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        private string password;
+        public string Password
+        {
+            get
+            {
+                return password;
+            }
+            set
+            {
+                password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        private List<tblUser> userList;
+        public List<tblUser> UserList
+        {
+            get
+            {
+                return userList;
+            }
+            set
+            {
+                userList = value;
+                OnPropertyChanged("UserList");
+            }
+        }
+
+        private tblUser user;
+        public tblUser User
+        {
+            get
+            {
+                return user;
+            }
+            set
+            {
+                user = value;
+                OnPropertyChanged("User");
+            }
+        }
+
+
+
+        #endregion
+
+        #region Commands
+        private ICommand login;
+        /// <summary>
+        /// Command login
+        /// </summary>
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {
+                    login = new RelayCommand(LoginExecute);
+                }
+                return login;
+            }
+            set { login = value; }
+        }
+        /// <summary>
+        /// Method for checking username and password
+        /// </summary>
+        /// <param name="o"></param>
+        private void LoginExecute(object o)
+        {
+            try
+            {
+                string pasword = (o as PasswordBox).Password;
+                if ( IsUniqueUsername(UserName) == true)
+                {
+                    MainWindow mw = new MainWindow();
+                    view.Close();
+                    mw.ShowDialog();
+                }
+
+
+                else
+                {
+                    MessageBox.Show("Incorrect username or password");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+
+        #endregion
+
+        public bool IsUniqueUsername(string username)
+        {
+            Service service = new Service();
+            List<tblUser> userList = service.GetAllUsers();
+            var list = userList.Where(x => x.Username == username).ToList();
+            if (list.Count() > 0)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+       
+    }
+}
+    
